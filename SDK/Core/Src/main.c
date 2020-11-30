b957f49e3302fe51a3d293d62718750b872a0081
JasonXY <1462357003@qq.com>
2020-11-30 16:16:07 +0800

    添加时钟配置介绍

--- SDK/Core/Src/main.c
+++ SDK/Core/Src/main.c
@@ -138,8 +138,8 @@ void SystemClock_Config(void)//输出100MHz时钟频率
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+  __HAL_RCC_PWR_CLK_ENABLE();//开启时钟
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1); //电压缩放
 
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
